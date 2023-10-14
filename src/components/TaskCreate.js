23243a1f737ae7a7e627eab3be9c84a971846171
Mansur Uyar <ymansuruyar@gmail.com>
2023-10-14 19:07:56 +0300

    FirstCommit
    starting a new project to remind from the beginning

---
+++ src/components/TaskCreate.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function TaskCreate() {
+  return (
+    <div className="task-create">
+      <h3>Lütfen Task Giriniz!</h3>
+      <form className="task-form">
+        <label className="task-label">Başlık</label>
+        <input className="task-input" />
+        <label className="task-label">Task Giriniz</label>
+        <textarea className="task-input" rows={5}/>
+        <button className="task-button">Oluştur</button>
+      </form>
+    </div>
+  );
+}
